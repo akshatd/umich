a5fc9ad51a4e79bc8fb48a8ff451f04eb12a5a4a
Akshat Dubey <akshat@transcelestial.com>
2023-11-03 17:20:24 -0400

    a4: split functions out into a separate file

---
+++ a4/functions.py
@@ -0,0 +1,72 @@
+import numpy as np
+
+# f_* are functions
+# h_* are equality constraints
+# g_* are inequality constraints
+# df, dh, dg are derivatives, will output jacobians
+
+
+# prob 4.2
+
+h = .25
+b = .125
+sigma_yield = 200000000
+tau_yeild = 116000000
+P = 100000
+l = 1
+
+
+def p42_f(x):
+    return 2*b*x[0] + h*x[1]
+
+
+def p42_df(x):
+    return np.array([2*b, h])
+
+
+def p42_I_inv(x):
+    return 768/(3*x[0] + 16*x[0]**3 + x[1])
+
+
+def p42_dI_inv(x):
+    u = (3*x[0] + 16*x[0]**3 + x[1])
+    du = np.array([3 + 48*x[0]**2, 1])
+    return -768*du/u**2
+
+
+def p42_g1(x):
+    return P*l*h*p42_I_inv(x)/2 - sigma_yield
+
+
+def p42_dg1(x):
+    return P*l*h*p42_dI_inv(x)/2
+
+
+def p42_g2(x):
+    return 1.5*P/(h*x[1]) - tau_yeild
+
+
+def p42_dg2(x):
+    return np.array([0, -1.5*P/(h*x[1]**2)])
+
+
+def p42_dL(vars):
+    x1, x2, sigma1, sigma2, s1, s2 = vars
+    x = [x1, x2]
+    dx = p42_df(x) + sigma1*p42_dg1(x) + sigma2*p42_dg2(x)
+    dsig1 = p42_g1(x) + s1**2
+    dsig2 = p42_g2(x) + s2**2
+    ds1 = 2*sigma1*s1
+    ds2 = 2*sigma2*s2
+
+    return [dx[0], dx[1], dsig1, dsig2, ds1, ds2]
+
+
+# prob 4.3
+
+
+if __name__ == "__main__":
+    # Initial guesses for variables
+    x_initial_guesses = [0.12345, 0.43534, 0.01231, 0.3242412, 1, 241]
+    # x_initial_guesses = [0.01212345, 12.43534, 0.0, 0.3242412, 1, 0]
+    print(p42_dL(x_initial_guesses))
--- a4/prob4_2.py
+++ a4/prob4_2.py
@@ -1,43 +1,10 @@
+#!/usr/bin/env python
+
 from scipy.optimize import fsolve
 import matplotlib.pyplot as plt
 import numpy as np
 
-h = .25
-b = .125
-sigma_yield = 200000000
-tau_yeild = 116000000
-P = 100000
-l = 1
-
-
-def func(x):
-    return 2*b*x[0] + h*x[1]
-
-
-def I(x):
-    return (3*x[0] + 16*x[0]**3 + x[1])/768
-
-
-def constraint1(x):
-    return P*l*h/(2*I(x)) - sigma_yield
-
-
-def constraint2(x):
-    return 1.5*P/(h*x[1]) - tau_yeild
-
-
-def diffeq(vars):
-    x1, x2, sigma1, sigma2, s1, s2 = vars
-    # s1 = 0
-    # s2 = 0
-    dx1 = 2*b - 384*sigma1*P*l*h*(3 + 48*x1**2)/(3*x1 + 16*x1**3 + x2)**2
-    dx2 = h - 384*sigma1*P*l*h / \
-        (3*x1 + 16*x1**3 + x2)**2 - 1.5*sigma2*P/(h*x2**2)
-    dsig1 = 384*P*l*h/(3*x1 + 16*x1**3 + x2) - sigma_yield + s1**2
-    dsig2 = 1.5*P/(h*x2) - tau_yeild + s2**2
-    ds1 = 2*sigma1*s1
-    ds2 = 2*sigma2*s2
-    return [dx1, dx2, dsig1, dsig2, ds1, ds2]
+import functions as fn
 
 
 def plot_constrained_opt(fx, constr1, constr2, opt, title):
@@ -72,8 +39,8 @@ if __name__ == "__main__":
     x_initial_guesses = [0.01, 0.01, 0, 0, 0, 0]
 
     # Use fsolve to solve the system of equations
-    solution = fsolve(diffeq, x_initial_guesses)
+    solution = fsolve(fn.p42_dL, x_initial_guesses)
     print('The solution of the system is ', solution)
 
-    plot_constrained_opt(func, constraint1, constraint2,
+    plot_constrained_opt(fn.p42_f, fn.p42_g1, fn.p42_g2,
                          solution[0:2], "Contour plot of the cross-sectional area with stress constraints")
