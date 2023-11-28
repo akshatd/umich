8bb1f7f8f4f04dcaf9f855f66bb28986217e98c3
Akshat Dubey <akshat@transcelestial.com>
2023-11-28 14:45:05 -0500

    fix output formatting

--- a6/prob6_2.py
+++ a6/prob6_2.py
@@ -1,20 +1,26 @@
 #!/usr/bin/env python
 
+import numpy as np
+
 import functions as fn
 from gradfree import nelder_mead, plot_nelder_mead
 
+
 if __name__ == "__main__":
-    x0 = [-1, 1.5]
+    np.set_printoptions(precision=15, sign=' ', suppress=True)
+    x0 = [-1.25, 1.5]
     # 6.2.a
+    print(f"6.2.a) Nelder-Mead optimum")
+    print("iters\toptimal x\t\t\t\toptimal fx")
     out = nelder_mead(fn.bean, x0, max_iter=100)
     xopt = out['simplex'][-1][0]
-    print(f"6.2.a) Nelder-Mead optimum in {out['iters']} iterations: {xopt}")
+    print(f"{out['iters']}\t{xopt}")
     plot_nelder_mead(fn.bean, out['simplex'],
                      "Nelder-Mead applied to the bean function")
 
     # 6.2.b
-    print(f"6.2.b) Nelder-Mead optimum with noise")
-    print("noise\titers\toptimum")
+    print(f"\n6.2.b) Nelder-Mead optimum with noise")
+    print("noise\titers\toptimal x\t\t\t\toptimal fx")
 
     noises = [10**(-exp) for exp in range(1, 10)]
     for noise in noises:
@@ -22,12 +28,12 @@ if __name__ == "__main__":
         out = nelder_mead(bean_noisy, x0, max_iter=100)
         xopt = out['simplex'][-1][0]
         print(f"{noise}\t{out['iters']}\t{xopt}")
-        # plot_nelder_mead(bean_noisy, out['simplex'],
-        #                  f"Nelder-Mead applied to the bean function with noise {noise}")
+        plot_nelder_mead(bean_noisy, out['simplex'],
+                         f"Nelder-Mead applied to the bean function with noise {noise}")
 
     # 6.2.c
-    print(f"6.2.c) Nelder-Mead optimum with checkerboard steps")
-    print("step\titers\toptimum")
+    print(f"\n6.2.c) Nelder-Mead optimum with checkerboard steps")
+    print("step\titers\toptimal x\t\t\t\toptimal fx")
 
     steps = [0.5*mult for mult in range(1, 10)]
     for step in steps:
