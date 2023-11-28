afebe7c01cf97465bb3a5c8df98f465f2d2d9eaf
Akshat Dubey <akshat@transcelestial.com>
2023-11-28 01:23:23 -0500

    add checkerboard steps

--- a6/functions.py
+++ a6/functions.py
@@ -22,14 +22,16 @@ def bean(x):
     return (1. - x1)**2 + (1. - x2)**2 + 0.5 * (2 * x2 - x1**2)**2
 
 
-def noisy_bean(x, noise):
+def bean_noisy(x, noise):
     """
-    Bean function from Appendix D of the book
+    Bean function from Appendix D of the book with noise
 
     Parameters
     ----------
     x : ndarray, shape (2,)
         design variables
+    noise : float
+        standard deviation of gaussian noise to be added
 
     Returns
     -------
@@ -42,3 +44,28 @@ def noisy_bean(x, noise):
 
     f = (1. - x1)**2 + (1. - x2)**2 + 0.5 * (2 * x2 - x1**2)**2
     return f + np.random.normal(0, noise)
+
+
+def bean_check(x, step):
+    """
+    Bean function from Appendix D of the book
+
+    Parameters
+    ----------
+    x : ndarray, shape (2,)
+        design variables
+    step : float
+        step for checkerboarding to be added
+
+    Returns
+    -------
+    f : float
+        function value
+    """
+
+    x1 = x[0]
+    x2 = x[1]
+
+    f = (1. - x1)**2 + (1. - x2)**2 + 0.5 * (2 * x2 - x1**2)**2
+    check = step*np.ceil(np.sin(np.pi*x1) * np.sin(np.pi*x2))
+    return f + check
--- a6/prob6_2.py
+++ a6/prob6_2.py
@@ -6,18 +6,34 @@ from gradfree import nelder_mead, plot_nelder_mead
 if __name__ == "__main__":
     x0 = [-1, 1.5]
     # 6.2.a
-    out = nelder_mead(fn.bean, x0, max_iter=1000)
+    out = nelder_mead(fn.bean, x0, max_iter=100)
     xopt = out['simplex'][-1][0]
     print(f"6.2.a) Nelder-Mead optimum in {out['iters']} iterations: {xopt}")
     plot_nelder_mead(fn.bean, out['simplex'],
                      "Nelder-Mead applied to the bean function")
 
     # 6.2.b
+    print(f"6.2.b) Nelder-Mead optimum with noise")
+    print("noise\titers\toptimum")
+
     noises = [10**(-exp) for exp in range(1, 10)]
     for noise in noises:
-        out = nelder_mead(lambda x: fn.noisy_bean(x, noise), x0, max_iter=100)
+        def bean_noisy(x): return fn.bean_noisy(x, noise)
+        out = nelder_mead(bean_noisy, x0, max_iter=100)
         xopt = out['simplex'][-1][0]
-        print(
-            f"6.2.b) Nelder-Mead optimum in {out['iters']} iterations with noise {noise}: {xopt}")
-        plot_nelder_mead(fn.bean, out['simplex'],
-                         f"Nelder-Mead applied to the bean function with noise {noise}")
+        print(f"{noise}\t{out['iters']}\t{xopt}")
+        # plot_nelder_mead(bean_noisy, out['simplex'],
+        #                  f"Nelder-Mead applied to the bean function with noise {noise}")
+
+    # 6.2.c
+    print(f"6.2.c) Nelder-Mead optimum with checkerboard steps")
+    print("step\titers\toptimum")
+
+    steps = [0.5*mult for mult in range(1, 10)]
+    for step in steps:
+        def bean_check(x): return fn.bean_check(x, step)
+        out = nelder_mead(bean_check, x0, max_iter=100)
+        xopt = out['simplex'][-1][0]
+        print(f"{step}\t{out['iters']}\t{xopt}")
+        plot_nelder_mead(bean_check, out['simplex'],
+                         f"Nelder-Mead applied to the bean function with step {step}")
